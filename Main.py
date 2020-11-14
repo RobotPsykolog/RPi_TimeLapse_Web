e152646cb2227a56749d5883fcfc2e2c7c3ec97d
Björn Persson <bjorn_persson@live.se>
2020-11-14 21:07:10 +0100

    Tränat på GIT bara. Lagt till och tagit bort filer.

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
+
 from app import app
 
 if __name__ == '__main__':
 
-    app.run(debug=True)    
+    #app.run(debug = True, host='0.0.0.0', extra_files = 'static/css')    
+    app.run(debug = True, host='0.0.0.0')    
+
+
--- app/views.py
+++ app/views.py
@@ -7,7 +7,20 @@ from flask import render_template
 @app.route('/index')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 
-    
+@app.route('/program1')
+def program1():
+    return render_template('program1.html')
+
+@app.route('/program2')
+def program2():
+    return render_template('program2.html')
+
+
+# För att cachen ska laddas om utan shift
+@app.after_request
+def apply_caching(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    return response
